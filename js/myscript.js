97df63d93248946bf1d050ba9fca2a38c5523f12
Emilo <emilio.furnari87@gmail.com>
2020-11-27 15:33:54 +0100

    seconda parte fatta

--- js/myscript.js
+++ js/myscript.js
@@ -3,11 +3,61 @@
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
+// PRIMO ESERCIZIO
+// CREO OGGETTO
+console.log("PRIMO ESERCIZIO");
 var studente = {
   nome: "emilio",
   cognome: "furnari",
   eta: 33,
 }
+// UTILIZZO UN CICLO FOR PER STAMPARE TUTTO IL CONTENUTO DELL'OGGETTO
 for (var proprieta in studente) {
   console.log(studente[proprieta]);
 }
+
+console.log("SECONDO ESERCIZIO");
+// CREO ARRAY DI OGGETTI
+var studenti =[
+  {
+    nome: "emilio",
+    cognome: "furnari",
+    eta: 33,
+  },
+  {
+    nome: "pippo",
+    cognome: "rossi",
+    eta: 33,
+  },
+  {
+    nome: "topolino",
+    cognome: "bianchi",
+    eta: 85,
+  },
+  {
+    nome: "paperino",
+    cognome: "duck",
+    eta: 80,
+  },
+  {
+    nome: "pietro",
+    cognome: "gambadilegno",
+    eta: 80,
+  },
+];
+// CERCO NELL'ARRAY
+
+for (var proprieta in studenti) {
+ console.log(studenti[proprieta].nome + " " + studenti[proprieta].cognome);
+}
+
+var nome = prompt("Scrivi il tuo nome?");
+var cognome = prompt("qual'è il tuo cognome?");
+var eta = parseInt(prompt("quanti anni hai?"));
+
+
+studenti.push(nome);
+studenti.push(cognome);
+studenti.push(eta);
+
+console.log(studenti);
